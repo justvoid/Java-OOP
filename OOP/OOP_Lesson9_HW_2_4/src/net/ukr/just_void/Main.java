34586eb4d2626ab2fe21f6b884c67ec2220f0545
just_void <just_void@ukr.net>
2018-11-21 18:19:25 +0200

    Added method for double values

--- OOP/OOP_Lesson9_HW_2_4/src/net/ukr/just_void/Main.java
+++ OOP/OOP_Lesson9_HW_2_4/src/net/ukr/just_void/Main.java
@@ -11,18 +11,29 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			System.out.println(findLargestNumber("a.txt"));
+			System.out.println(findLargestNumberDouble("b.txt"));	
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public static double findLargestNumberDouble(String fileName) throws IOException{
+		Double output = Files.lines(Paths.get(fileName))
+				.mapToDouble(n -> Arrays.stream(n.split(" "))
+						.mapToDouble(o -> Double.valueOf(o))
+						.max().getAsDouble())
+				.max().getAsDouble();
+		return output;
+	}
+
+	
 	public static int findLargestNumber(String fileName) throws IOException{
-		Optional<Integer> output = Files.lines(Paths.get(fileName))
+		int output = Files.lines(Paths.get(fileName))
 				.map(n -> Arrays.stream(n.split(" "))
 						.map(o -> Integer.valueOf(o))
 						.max((o1, o2) -> o1 - o2).get())
-				.max((o1, o2) -> o1 - o2);
-		return output.get();
+				.max((o1, o2) -> o1 - o2).get();
+		return output;
 	}
 
 }
